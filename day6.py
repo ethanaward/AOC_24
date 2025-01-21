699cb456afbe2e4167215ffea2177f8712577f0e
Ethan Ward <ethanandrewward@gmail.com>
2025-01-21 16:40:08 -0600

    Finish 6.2, checking for a loop could be cleaner

--- day6.py
+++ day6.py
@@ -5,11 +5,13 @@ class Guard:
     direction = "u"
     x = 0
     y = 0
+    turns = {}
 
     def __init__(self, direction, x, y):
         self.direction = direction
-        self.x = x
-        self.y = y
+        self.x = int(x)
+        self.y = int(y)
+        self.turns = {}
 
     def forward(self):
         match self.direction:
@@ -26,11 +28,19 @@ class Guard:
             case "l": self.y += 1
 
     def turn(self):
+        if (self.x, self.y) in self.turns:
+            self.turns[(self.x, self.y)] += 1
+        else:
+            self.turns[(self.x, self.y)] = 1
         match self.direction:
             case "u": self.direction = "r"
             case "r": self.direction = "d"
             case "d": self.direction = "l"
             case "l": self.direction = "u"
+
+    # This is an inefficient way to determine this, but something cleaner wasn't coming to mind.
+    def is_looped(self):
+        return len([x for x in self.turns if self.turns[x] > 20]) > 0
 
 
 np_arr = np.loadtxt('input/input6.txt', dtype=np.str_, comments='%')
@@ -41,14 +51,37 @@ starting_point = np.where(np_matrix == '^')
 
 guard = Guard('u', starting_point[0], starting_point[1])
 visited_pos = []
-# visited_pos = np.full_like(np_matrix, '-', dtype=np.str_)
 while 0 <= guard.x < len(np_matrix) and 0 <= guard.y < len(np_matrix):
-    # visited_pos[guard.x, guard.y] = 'X'
     if np_matrix[(guard.x, guard.y)] == '#':
-        # visited_pos[(guard.x, guard.y)] = '-'
         guard.backward()
         guard.turn()
-    visited_pos.append((guard.x[0], guard.y[0]))
+    visited_pos.append((guard.x, guard.y))
     guard.forward()
 print("The guard has visited {} positions.".format(len(set(visited_pos))))
 
+
+# Part 2
+def test_position(layout, new_guard):
+    while 0 <= new_guard.x < len(layout) and 0 <= new_guard.y < len(layout) and not new_guard.is_looped():
+        if layout[(new_guard.x, new_guard.y)] == '#':
+            new_guard.backward()
+            new_guard.turn()
+        visited_pos.append((new_guard.x, new_guard.y))
+        new_guard.forward()
+    return new_guard.is_looped()
+
+
+possible_blocks = 0
+# You only need to check putting blocks in places the guard visits, since that's the only way the
+# path of the guard will change.
+for position in set(visited_pos):
+    new_matrix = np_matrix.copy()
+    new_matrix[position] = '#'
+    guard = Guard('u', starting_point[0], starting_point[1])
+    looped = test_position(new_matrix, guard)
+    if looped:
+        # print(new_matrix)
+        possible_blocks += 1
+
+
+print("There are {} possible locations to loop the guard.".format(possible_blocks))
